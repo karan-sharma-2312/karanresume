6b2f17124962b8d21732733b107f12f0602a0187
Karan Sharma <81227234+karan-sharma-2312@users.noreply.github.com>
2021-09-01 00:52:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
             $('.navbar').removeClass("sticky");
         }
         
-        // scroll-up button show/hide script
+        // scroll-up button show
         if(this.scrollY > 500){
             $('.scroll-up-btn').addClass("show");
         }else{
@@ -47,28 +47,5 @@ $(document).ready(function(){
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
-    });
-
-    // owl carousel script
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplay: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0:{
-                items: 1,
-                nav: false
-            },
-            600:{
-                items: 2,
-                nav: false
-            },
-            1000:{
-                items: 3,
-                nav: false
-            }
-        }
     });
 });
